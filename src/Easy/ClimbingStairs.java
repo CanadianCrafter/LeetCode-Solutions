3a9b23c9f20fb893730a42d196241cc8f3c3d0c7
CanadianCrafter <bryan335667879@gmail.com>
2023-07-23 11:27:17 -0400

    Climbing Stairs

---
+++ src/Easy/ClimbingStairs.java
@@ -0,0 +1,47 @@
+package Easy;
+
+import java.io.*;
+import java.util.*;
+
+public class ClimbingStairs {
+    
+    static InputStreamReader is = new InputStreamReader(System.in);
+    static BufferedReader br = new BufferedReader(is);
+    static StringTokenizer st;
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    	
+    public static void main(String args[]) throws IOException {
+    	
+    }
+    public static int climbStairs(int n) {
+    	//Using generating function for a fib series
+        return (int) Math.round( ((double)(5+Math.sqrt(5))/10) *  Math.pow(((double)(1+Math.sqrt(5))/2),n));
+    }
+    
+    
+	static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine().trim());
+        }
+        return st.nextToken();
+    }
+
+    static long readLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int readInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double readDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static String readLine() throws IOException {
+        return br.readLine().trim();
+    }
+    
+}
+
+

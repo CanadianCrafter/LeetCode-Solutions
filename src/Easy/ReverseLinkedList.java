e6e4f0e9de72c06592f4d2014ac40c2363eac371
CanadianCrafter <bryan335667879@gmail.com>
2023-08-03 21:13:12 -0400

    reverse linked list

---
+++ src/Easy/ReverseLinkedList.java
@@ -0,0 +1,63 @@
+package Easy;
+
+import java.io.*;
+import java.util.*;
+
+public class ReverseLinkedList {
+    
+    static InputStreamReader is = new InputStreamReader(System.in);
+    static BufferedReader br = new BufferedReader(is);
+    static StringTokenizer st;
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    	
+    public static void main(String args[]) throws IOException {
+        
+       
+    }
+    public ListNode reverseList(ListNode head) {
+		ListNode cur = head;
+		ListNode prev = null;
+		ListNode next = null;
+
+		while(cur!=null) {
+			next = cur.next;
+			cur.next = prev;
+			prev = cur;
+			cur = next;
+		}
+		head = prev;
+		return head;
+        
+    }
+	static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine().trim());
+        }
+        return st.nextToken();
+    }
+
+    static long readLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int readInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double readDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static String readLine() throws IOException {
+        return br.readLine().trim();
+    }
+    
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      }
+
+}

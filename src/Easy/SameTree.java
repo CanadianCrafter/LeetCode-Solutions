6d6ce043a1b3a1cac0b4070e6b144b948bf5e843
CanadianCrafter <bryan335667879@gmail.com>
2023-10-01 16:47:29 +0800

    same tree

---
+++ src/Easy/SameTree.java
@@ -0,0 +1,62 @@
+package Easy;
+
+import java.io.*;
+import java.util.*;
+
+public class SameTree {
+    
+    static InputStreamReader is = new InputStreamReader(System.in);
+    static BufferedReader br = new BufferedReader(is);
+    static StringTokenizer st;
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    
+    
+    public static void main(String args[]) throws IOException {
+        TreeNode a = new TreeNode (1, new TreeNode(2, null, null), new TreeNode(3,null,null));
+        TreeNode b = new TreeNode (1, new TreeNode(25, null, null), new TreeNode(3,null,null));
+        System.out.println(isSameTree(a,b));
+       
+    }
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null&&q==null) return true;
+        if(p==null||q==null) return false;
+        if(p.val!=q.val) return false;
+        
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+	static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine().trim());
+        }
+        return st.nextToken();
+    }
+
+    static long readLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int readInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double readDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static String readLine() throws IOException {
+        return br.readLine().trim();
+    }
+    
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) { 
+        	this.val = val; 
+        	this.left = left; 
+        	this.right = right;
+        }
+     }
+}
